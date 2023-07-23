c994119c6de5c83a0b0588aee6b24eb74e216641
dzc0d3r <m4dcod3r@protonmail.com>
2023-07-23 02:17:13 +0100

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+const divide = function (number1, number2) {
+    return number2 != 0 ? number1 / number2 : "Cannot divide by zero"
+}
+
+
+
+const square = number => number ** 2
+
+
+const add = (number1, number2) => number1 + number2
